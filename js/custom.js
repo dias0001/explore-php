830c5730fd947294e1d0ad12c535958013ef5800
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-10-26 11:40:46 -0300

    update

--- js/custom.js
+++ js/custom.js
@@ -1,136 +1,128 @@
+/*  ///////////////////  FUNÇÕES   ////////////////////  */
 
-/* ///////////// funções /////////////////////////////////////////// */
-const aviso = (msg) => {
+//Ao carregar a página verifica o parâmetro "cadastro"
+//se o valor for "ok" exibe um alert
+const urlParams = new URLSearchParams(window.location.search)
+var parCadastro =  urlParams.get('cadastro')
+if(parCadastro == 'ok'){
+  let listaClasses = document.getElementById('msg').classList
+  listaClasses.remove('d-none')
+}
+
+const aviso = (msg)=> {
     alert(msg)
 }
 
-const mostraidade = () => {
+const mostraIdade = ()=>{
     let span = document.getElementById('txt-idade')
-    let campoidade = document.getElementById('idade')
-    span.innerText = campoidade.value
+    let campoIdade = document.getElementById('idade')
+    span.innerText = campoIdade.value
 }
 
 var dataAtual = new Date()
-const mostraData = () => {
-    let dia = dataAtual.getDay()
-    let mes = dataAtual.getMonth() + 1
-    let ano = dataAtual.getFullYear()
-    let hora = dataAtual.getHours()
-    let valor = dia + '/' + mes + '/' + ano
-    document.getElementById('dt-cadastro').value = valor
+const monstraData = ()=> {
+  let dia = dataAtual.getDay()
+  let mes = dataAtual.getMonth() + 1
+  let ano = dataAtual.getFullYear()
+  let hora = dataAtual.getHours()
+  let valor = dia +'/'+ mes + '/' + ano
+
+  document.getElementById('dt-cadastro').value = valor
 }
 
 //Preenche o select "estado" com os estados da API do IBGE
-const getEstados = () => {
-    let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-    let select = document.getElementById('estado')
+const getEstados = ()=>{
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
 
-    //Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
-    fetch(api).then(resposta => resposta.json()).then(json => {
-        let options = '<option>Selecione</option>'
+  //Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
 
-        /* console.log(json) */
-        for (const index in json) {
-            // console.log(json[index].nome)
-            options += `<option value="${json[index].sigla}">${json[index].nome}</option>`
-        }
-        select.innerHTML = options
-    })
+    //Percorre o objeto JSON com os estados do Brasil
+    for (const index in json) {
+      options += `<option value=${json[index].sigla}>${json[index].nome}</option>`
+    }    
+    select.innerHTML = options
+  })
 }
 
-// preencher o select de cidades de acordo com UF selecionado 
-// a função recebe um parâmetro (uf) com ID da UF 
-const getCidadesbyUF = (uf) => {
-    let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
-    let select = document.getElementById('cidade')
 
-    fetch(api).then(resposta => resposta.json()).then(json => {
-        let options = '<option>Selecione</option>'
-
-        for (const index in json) {
-            options += `<option value="${json[index].nome}">${json[index].nome}</option>`
-        }
-        select.innerHTML = options
-    })
+//Preenche o select de cidades de acordo com o UF selecionado
+//A função recebe um parâmetro (uf) com a sigla da UF
+const getCidadesByUf = (uf)=>{
+  let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+  let select = document.getElementById('cidade')
+  
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
+    
+    for (const index in json) {
+      options += `<option value=${json[index].nome}>${json[index].nome}</option>`
+    }    
+    select.innerHTML = options
+  })  
 }
 
 const rolagem = ()=> {
-    const html = document.documentElement
-    const seta = document.getElementById('go-top')
-  
-    /* Se a rolagem for maior que 550, a seta aparece 
-    abaixo de 550 esconde */
-    if (html.scrollTop > 550 ) {
-      seta.style.display = 'block'
-    } else {
-      seta.style.display = 'none'
-    }
+  const html = document.documentElement
+  const seta = document.getElementById('go-top')
+
+  /* Se a rolagem for maior que 550, a seta aparece 
+  abaixo de 550 esconde */
+  if (html.scrollTop > 550 ) {
+    seta.style.display = 'block'
+  } else {
+    seta.style.display = 'none'
   }
-
-/*  EXEMPLO LAÇO FOR
-
-var semestre = ['jan', 'fev', 'Mar', 'Abr', 'Mai', 'Jun']
-var texto = ''
-for (let index = 0; index < semestre.length; index++) {
-const element = semestre[index];
-texto += element + '<br>'
-document.getElementById('explorar').innerHTML = texto 
-
-} */
-
-/* ---------------------------------------------------------------- */
-
-/* //////////////////////// eventos e execuções automaticas /////  */
-
-/*document.getElementById('nome').addEventListener('click', aviso)
-aviso('Tenha um bom dia Dave')
-document.getElementById('nome').addEventListener('click' , function(){
-    aviso('olá mundo !!! Este é o planeta terra')
-}) */
+}
 
 
+/* ---------------------------------------------------- */
 
-mostraidade()
-document.getElementById('idade').addEventListener('change', mostraidade)
 
-mostraData()
+/* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
 
 getEstados()
 
+mostraIdade()
+document.getElementById('idade').addEventListener('change', mostraIdade)
 
-//inicializada animações scrool do AOS
+monstraData()
+
+//Inicializa animações scroll do AOS
 AOS.init();
 
 // Impede o envio do formulário quando os campos estão inválidos
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+/* ( ()=> {
     'use strict'
-
-    //váriavel captura as tags <form> que contém a classe "nedds-validation"    
-    const forms = document.querySelectorAll('.needs-validation')
-
-
+  
+    //váriavel captura as tags <form> que contém a classe "needs-validation"
+    var forms = document.querySelectorAll('.needs-validation')
+  
     // Executa para cada formulário da variável forms
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            //Se houver campos inválidos, interrompe o SUBMIT
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-
-            form.classList.add('was-validated')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          //Se houver campos inválidos, interrompe o SUBMIT
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
         }, false)
-    })
-})()
+      })
+  })() */
 
-// this representa o objeto
-document.getElementById('estado').addEventListener('change', function () {
-    getCidadesbyUF(this.value)
+
+document.getElementById('estado').addEventListener('change', function(){
+  getCidadesByUf(this.value)
 })
 
-//window.addEventListener('scroll , rolagem')
+//window.addEventListener('scroll', rolagem)
+window.onscroll = ()=> rolagem()
 
-window.onscroll = () => rolagem()
+
 
--- processa.php
+++ processa.php
@@ -1,10 +1,6 @@
 <?php
 
-
-/* phpinfo(); */
-
-
-//recebe os campos do formulário
+//Recebe os campos do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $sexo = $_POST['sexo'];
@@ -18,35 +14,39 @@ $hospedagem = $_POST['hospedagem'];
 $mensagem = $_POST['mensagem'];
 $dt_cadastro = $_POST['dt-cadastro'];
 
-
 //para investigar variáveis e expressões
 //var_dump($_POST);
 
-//conecta ao banco e grava os dados (insert com PDO)
+//CONECTA AO BANCO E GRAVA OS DADOS (insert com PDO)
 try {
-    // instancia o banco por meio do PDO
-    $pdo = new PDO('mysql:host=localhost;dbname=explore',  'root', '');
+    //instancia o banco por meio do PDO
+    $pdo = new PDO('mysql:host=localhost;dbname=explore', 'root', '');
     //INSERT na tabela users
-    $sql = $pdo->prepare('insert into users (nome, email, sexo, telefone, senha, idade, estado, cidade, destinos, hospedagem, mensagem, dt_cadastro) value(:nome, :email, :sexo, :telefone, :senha, :idade, :estado, :cidade, :destinos, :hospedagem, :mensagem, :dt_cadastro)');
+    $sql = $pdo->prepare('INSERT into users 
+    (nome, email, sexo, telefone, senha, idade, estado, cidade, 
+    destinos, hospedagem, mensagem, dt_cadastro)
+    values(:nome, :email, :sexo, :telefone, :senha, :idade, :estado, :cidade, 
+    :destinos, :hospedagem, :mensagem, :dt_cadastro)');
     $sql->execute(array(
         ':nome' => $nome,
-        ':email' => $email,
-        ':sexo'  => $sexo,
-        ':telefone'  => $telefone,
-        ':senha'  => md5($senha),
-        ':idade'  => $idade,
-        ':estado'  => $estado,
-        ':cidade'  => $cidade,
-        ':destinos'  => $destinos,
-        ':hospedagem'  => $hospedagem,
-        ':mensagem'  => $mensagem,
-        ':dt_cadastro'  => date('Y-m-d', strtotime($dt_cadastro))
+        ':email' => $email, 
+        ':sexo' => $sexo, 
+        ':telefone' => $telefone, 
+        ':senha'=> md5($senha), 
+        ':idade'=> $idade, 
+        ':estado'=> $estado, 
+        ':cidade'=> $cidade,
+        ':destinos'=> implode(',', $destinos), //converte array em texto 
+        ':hospedagem'=> $hospedagem, 
+        ':mensagem'=> $mensagem, 
+        ':dt_cadastro'=> date('Y-m-d', strtotime($dt_cadastro))
     ));
 
-    echo '<h1>Usuário cadastrado</h1>';
-    var_dump($_POST);
+    //carrega a página index.html enviando variável GET cadastro
+    header('Location: index.html?cadastro=ok');   
+    
 
-} catch (PDOException  $erro) {
+} catch (PDOException $erro) {
     //se der erro, exibe o erro aqui
-    echo $erro;    
-}
+    echo $erro;
+} 
